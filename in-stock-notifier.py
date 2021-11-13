477cf6716825ab78b9f6fe4f42023ae6b440591d
NaimShaqqou <naeemshaqqou@gmail.com>
2021-11-13 14:33:10 -0500

    testing selenium

--- in-stock-notifier.py
+++ in-stock-notifier.py
@@ -1,7 +1,7 @@
-import selenium
+from selenium import webdriver
 
 # Using Chrome to access the internet
-driver = webdriver.Chrome()
+driver = webdriver.Chrome("./chromedriver.exe")
 
 # open the webpage in question
-driver.get("website name")
+driver.get("https://www.google.com/")
