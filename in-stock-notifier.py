d257957644d49f341226efe76f3b2b03155066d4
NaimShaqqou <naeemshaqqou@gmail.com>
2021-11-13 16:21:46 -0500

    search and click on element implemented

--- in-stock-notifier.py
+++ in-stock-notifier.py
@@ -2,10 +2,32 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
 # Using Chrome to access the internet
 ser = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service = ser)
+opt = Options()
+driver = webdriver.Chrome(service = ser, options=opt)
 
-# open the webpage in question
-driver.get("https://www.google.com/")
+# open the webpage for newegg.com
+driver.get("https://www.newegg.com/")
+driver.implicitly_wait(2)
+
+# search for the graphics card
+search_bar = driver.find_element(By.CSS_SELECTOR, "input[type='search']")
+driver.implicitly_wait(2)
+search_bar.send_keys("RTX 3060")
+search_bar.send_keys(Keys.RETURN)
+
+# click on all the results and as soon as one is found
+# send a notification
+while(True):
+    result = driver.find_element(By.PARTIAL_LINK_TEXT, 'GeForce RTX 3060')
+    result.click()
+    driver.implicitly_wait(10)
+    driver.back()
+    driver.implicitly_wait(10)
+
+while(True):
+    pass
