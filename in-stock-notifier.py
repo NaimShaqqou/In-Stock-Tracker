ef1ffeaff52160ce73c44565bbb46a50c6492e6a
NaimShaqqou <naeemshaqqou@gmail.com>
2021-11-13 14:54:04 -0500

    Successful use of selenium in python

--- in-stock-notifier.py
+++ in-stock-notifier.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
 # Using Chrome to access the internet
-driver = webdriver.Chrome("./chromedriver.exe")
+ser = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service = ser)
 
 # open the webpage in question
 driver.get("https://www.google.com/")
