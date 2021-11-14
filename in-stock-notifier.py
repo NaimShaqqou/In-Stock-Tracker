a4ef64a7b0fa175d77c6c9a1c1beab5adae76427
NaimShaqqou <naeemshaqqou@gmail.com>
2021-11-13 21:36:13 -0500

    split code into functions

--- in-stock-notifier.py
+++ in-stock-notifier.py
@@ -6,23 +6,30 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
 # Using Chrome to access the internet
-ser = Service(ChromeDriverManager().install())
-opt = Options()
-driver = webdriver.Chrome(service = ser, options=opt)
+def run_chrome() :
+    ser = Service(ChromeDriverManager().install())
+    opt = Options()
+    driver = webdriver.Chrome(service = ser, options=opt)
 
-# open the webpage for newegg.com
-driver.get("https://www.newegg.com/")
-driver.implicitly_wait(2)
+# checks if product in given webpage is in stock
+def check_stock(webpage) :
+    # open the webpage in chrome
+    run_chrome()
+    driver.get(f"https://{webpage}.com")
+    driver.implicitly_wait(2)
 
-# search for the graphics card
-search_bar = driver.find_element(By.CSS_SELECTOR, "input[type='search']")
-driver.implicitly_wait(2)
-search_bar.send_keys("RTX 3060")
-search_bar.send_keys(Keys.RETURN)
+    # check which website we are on
 
-# click on all the results and as soon as one is found
-# send a notification
 
-result = driver.find_element(By.PARTIAL_LINK_TEXT, 'GeForce RTX 3060')
-result.click()
-driver.implicitly_wait(10)
+    # search for the graphics card
+    search_bar = driver.find_element(By.CSS_SELECTOR, "input[type='search']")
+    driver.implicitly_wait(2)
+    search_bar.send_keys("RTX 3060")
+    search_bar.send_keys(Keys.RETURN)
+
+    # click on all the results and as soon as one is found
+    # print to discord
+
+    result = driver.find_element(By.PARTIAL_LINK_TEXT, 'GeForce RTX 3060')
+    result.click()
+    driver.implicitly_wait(10)
