78faed36a7c7ae47ae32f6e1ee55bbd595b5dc32
MisaelSalzr <misaels@knights.ucf.edu>
2021-11-13 22:31:52 -0500

    Added Discord bot

--- discord-bot.py
+++ discord-bot.py
@@ -1,9 +1,12 @@
 import discord
 import random
+import importlib
+importlib.import_module('in-stock-notifier.py')
 from discord.ext import commands
 
-TOKEN = 'OTA5MTc3OTQwMzk3NDEyMzky.YZAf8A.neUkaL_TepA-jiRIjJB3khUIPWQ'
+TOKEN = 'OTA5MTc3OTQwMzk3NDEyMzky.YZAf8A.uSI6IPdexRQfxPz5TMNYDQg70RU'
 tracking_list = []
+valid_sites = ["amazon", "newegg", "ebay", "walmart", "bestbuy"]
 
 client = commands.Bot(command_prefix='')
 discord.Intents.all()
@@ -26,15 +29,28 @@ async def on_member_remove(ctx, member):
 
 @client.command()
 async def track(ctx, message):
-    if 'https://' in message:
+    check = message.split(" ", 1)
+    if check[0] in valid_sites:
         await ctx.send(f"Now tracking: {message}")
-        tracking_list.append(message)
+        tracking_list.append(check[1])
+        check_stock(check[0], check[1])
+    else:
+        await ctx.send("Invalid input.")
+
+
+@client.command()
+async def price(ctx, message):
+    for i in valid_sites:
+        # await ctx.send(f'Site: {i} \nPrice: {check_price(i, message)}\n')
+        print()
 
 
 @client.command()
 async def List(ctx):
+    num = 1;
     for i in tracking_list:
-        await ctx.send(f'{i}\n')
+        await ctx.send(f'{num}: {i}\n')
+        num += 1
 
 
 @client.command()
