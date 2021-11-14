ebbeb34d0c5759a490edeed8a8f620d18eaf2a18
MisaelSalzr <misaels@knights.ucf.edu>
2021-11-14 00:41:40 -0500

    Integrated selenium websearch and discord bot functionality. Had to rename a file to fix import statements, and completed track command

--- discord-bot.py
+++ discord-bot.py
@@ -1,10 +1,11 @@
 import discord
 import random
-import importlib
-importlib.import_module('in-stock-notifier.py')
+import notifier
+
 from discord.ext import commands
 
-TOKEN = 'OTA5MTc3OTQwMzk3NDEyMzky.YZAf8A.uSI6IPdexRQfxPz5TMNYDQg70RU'
+# For security purposes, removed token for gitHub uploads
+TOKEN = ''
 tracking_list = []
 valid_sites = ["amazon", "newegg", "ebay", "walmart", "bestbuy"]
 
@@ -12,6 +13,12 @@ client = commands.Bot(command_prefix='')
 discord.Intents.all()
 
 
+# client events represent actions the bot takes once it recognizes
+# certain events have occured, EX: on_ready prints to console once
+# the bot recognizes it has succesfully connected to discord
+#######################################################################
+# These events represent introductory functionality to get me
+# acclimated to working with discord bots
 @client.event
 async def on_ready():
     print('Bot is ready.')
@@ -19,40 +26,51 @@ async def on_ready():
 
 @client.event
 async def on_member_join(ctx, member):
-    await ctx.send(f'{member}, welcome to my server!')
+    await ctx.send(f'Welcome, {member}, to my server!')
 
 
 @client.event
 async def on_member_remove(ctx, member):
-    await ctx.send(f'{member}, goodbye!')
+    await ctx.send(f'Goodbye, {member}!')
 
+##########################################################################
 
+# client commands represent additional functionality accessible
+# to the discord users, EX: roll returns an int [1 - 6] inclusive
+
+# track calls the check stock on a valid website for an input item
 @client.command()
-async def track(ctx, message):
-    check = message.split(" ", 1)
-    if check[0] in valid_sites:
-        await ctx.send(f"Now tracking: {message}")
-        tracking_list.append(check[1])
-        check_stock(check[0], check[1])
+async def track(ctx, *, message):
+    checklist = message.split(" ", 1)
+
+    if checklist[0] in valid_sites:
+        await ctx.send(f"Now searching for {checklist[1]} on {checklist[0]}.")
+        tracking_list.append(checklist[1])
+        notifier.check_stock(checklist[0], checklist[1])
     else:
-        await ctx.send("Invalid input.")
+        await ctx.send('Invalid input: please try again')
 
 
+# price calls a possible check price method for an input item
 @client.command()
-async def price(ctx, message):
+async def price(ctx, *, message):
     for i in valid_sites:
         # await ctx.send(f'Site: {i} \nPrice: {check_price(i, message)}\n')
         print()
 
 
+# list prints all input items currently being tracked
 @client.command()
-async def List(ctx):
+async def list(ctx):
     num = 1;
     for i in tracking_list:
         await ctx.send(f'{num}: {i}\n')
         num += 1
 
 
+##############################################################
+# these methods represent introductory functionality to get
+# me acclimated to working with discord bots
 @client.command()
 async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
@@ -67,5 +85,6 @@ async def clear(ctx, amount=10):
 async def roll(ctx):
     await ctx.send(f'Roll: {random.randint(1, 6)}')
 
+#############################################################
 
 client.run(TOKEN)
